f0b6b1931eede879ce770a396d26c7752d7993c7
David P. <megver83@parabola.nu>
2024-10-31 15:48:47 -0300

    Ser the mongo URI through env var

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { CourseModule } from './course/course.module';
 import { UnitModule } from './unit/unit.module';
@@ -11,7 +12,8 @@ import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://root:example@localhost:27017/online-courses-platform?authSource=admin'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
     UsersModule,
     CourseModule,
     UnitModule,
